58471a618d9a90780bb8bfe5aff998823e4c0ac0
Vivek Swarnkar <vivekswarnkar18@gmail.com>
2026-08-18 19:35:25 +0530

    Time: 9 ms (31.17%) | Memory: 12.3 MB (93.12%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -10,7 +10,7 @@ class Solution(object):
                 if board[i][j]==".":
                     continue
                 row=board[i][j]+"row"+str(i)
-                column=board[i][j]+"column"+str(j)
+                column=board[i][j]+"column" + str(j)
                 box=board[i][j]+"box"+str(i//3)+str(j//3)
                 if row in st or column in st or box in st:
                     return False
@@ -18,4 +18,6 @@ class Solution(object):
                 st.add(column)
                 st.add(box)
         return True
+
+                
         
