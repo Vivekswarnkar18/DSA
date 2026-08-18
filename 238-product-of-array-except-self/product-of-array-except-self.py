2fba67e9be6386b7c465c7f41b61f3b9dfa2755f
Vivek Swarnkar <vivekswarnkar18@gmail.com>
2026-08-18 19:22:52 +0530

    Time: 40 ms (31.64%) | Memory: 23.6 MB (8.32%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -8,10 +8,11 @@ class Solution(object):
         left=[1]*n
         right=[1]*n
         for i in range(1,n):
-            left[i]=left[i-1]*nums[i-1]
+            left[i]=nums[i-1]*left[i-1]
         for i in range(n-2,-1,-1):
-            right[i]=right[i+1]*nums[i+1]
+            right[i]=nums[i+1]*right[i+1]
         ans=[1]*n
         for i in range(n):
-            ans[i]=left[i]*right[i]
+            ans[i]=right[i]*left[i]
         return ans
+        
